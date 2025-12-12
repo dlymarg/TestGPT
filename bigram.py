0af2b1b2a2215ee24a1c1e97dd161550e6a3fffb
Derek Margulies <dlymarg@gmail.com>
2025-12-11 22:39:21 -0500

    added comments

--- bigram.py
+++ bigram.py
@@ -8,12 +8,14 @@ block_size = 8 # what is the maximum context length for predictions?
 max_iters = 3000
 eval_interval = 300
 learning_rate = 1e-2
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cuda' if torch.cuda.is_available() else 'cpu' # leverage dedicated GPU, if available
 eval_iters = 200
 # ------------
 
+# for reproducibility
 torch.manual_seed(1337)
 
+# read the data
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
@@ -27,23 +29,25 @@ itos = { i:ch for i,ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
 decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 
-# Train and test splits
+# create train and test splits
 data = torch.tensor(encode(text), dtype=torch.long)
 n = int(0.9*len(data)) # first 90% will be train, rest val
 train_data = data[:n]
 val_data = data[n:]
 
 # data loading
+# get batch of inputs and targets
 def get_batch(split):
     # generate a small batch of data of inputs x and targets y
     data = train_data if split == 'train' else val_data
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([data[i:i+block_size] for i in ix])
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-    x, y = x.to(device), y.to(device)
+    x, y = x.to(device), y.to(device) # move to GPU
     return x, y
 
-@torch.no_grad()
+# averages up loss over multiple batches
+@torch.no_grad() # telling pytorch that we will not call .backward() on this function (i.e., backpropagation)
 def estimate_loss():
     out = {}
     model.eval()
@@ -96,11 +100,12 @@ class BigramLanguageModel(nn.Module):
         return idx
 
 model = BigramLanguageModel(vocab_size)
-m = model.to(device)
+m = model.to(device) # move to GPU
 
 # create a PyTorch optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
+# this is the training loop
 for iter in range(max_iters):
 
     # every once in a while evaluate the loss on train and val sets
